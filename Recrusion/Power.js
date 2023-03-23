945cf695366a02a46b272fc7ee78c7f623da531d
Bhavishm009 <bhavishm009@gmail.com>
2023-03-23 23:07:05 +0530

    added

---
+++ Recrusion/Power.js
@@ -0,0 +1,23 @@
+function Power(n){
+    if(n==0){
+        return 1
+    }
+    else if(n<1 || n % 2 == 0){
+        return false
+    }
+    else{
+        return Power(n/2)
+    }
+}
+console.log(Power(16))
+
+
+
+
+// function pow(b,e){
+//     if(e == 0 ){
+//         return 1
+//     }
+//     return  b * pow(b,e-1)
+// }
+// console.log(pow(3,5))
