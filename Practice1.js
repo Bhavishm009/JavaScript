4d035b79ab30604e8c306ac00c987a30240ccc56
Bhavishm009 <bhavishm009@gmail.com>
2023-03-29 09:46:17 +0530

    Reverse

--- Practice1.js
+++ Practice1.js
@@ -10,3 +10,27 @@
 // console.log(Pal('madam'))
 
 
+// function ReverseArr(arr){
+//  let i = 0 ; let j = arr.length-1;
+//  while(i<j){
+//     arr[i] = arr[i] + arr[j]
+//     arr[j] = arr[i] - arr[j]
+//     arr[i] = arr[i] - arr[j]
+//     i++
+//     j--
+//  }
+//  return arr
+// }
+// console.log(ReverseArr([1,2,3,4,5]))
+
+function RemoveDup(arr){
+    let unique = []
+ for(let i =0;i<arr.length;i++){
+    if(unique.indexOf(arr[i])=== -1){
+        unique.push(arr[i])
+    }
+ }
+ return unique
+
+}
+console.log(RemoveDup([1,1,2,2,2.3,4,5,5,6]))
--- Swap/AngramPalindrome.js
+++ Swap/AngramPalindrome.js
@@ -1,27 +1,28 @@
-let s="ABOBANOCCY";
+let s = "ABOBANOCCY";
 
-var mp=new Map()
+var mp = new Map();
 
-for(let i=0;i<s.length;i++){
-    let x=s[i]
-    if(mp.has(x)){//already present
-        mp.set(x,mp.get(x)+1)
-    }
-    else{//not present
-        mp.set(x,1);
-    }
+for (let i = 0; i < s.length; i++) {
+  let x = s[i];
+  if (mp.has(x)) {
+    //already present
+    mp.set(x, mp.get(x) + 1);
+  } else {
+    //not present
+    mp.set(x, 1);
+  }
 }
 
-let c=0;
-mp.forEach((value,key)=>{
-    if(value%2==1){//odd
-        c++;
-    }
-})
+let c = 0;
+mp.forEach((value, key) => {
+  if (value % 2 == 1) {
+    //odd
+    c++;
+  }
+});
 
-if(c>1){
-    console.log("NO");
-}
-else{
-    console.log("YES");
+if (c > 1) {
+  console.log("NO");
+} else {
+  console.log("YES");
 }
